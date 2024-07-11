6e5957d4627514d79e9e4a4a1263e986f8ae472c
Huking8 <huking850@gmail.com>
2024-07-11 13:43:41 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
-travaSend: process.env.TRAVA_SEND || '25' ,   
+SESSION_ID: process.env.SESSION_ID || '0cda417e-dd08-439c-9326-9687f7cdfb05' ,
+travaSend: process.env.TRAVA_SEND || '25' ,  
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
